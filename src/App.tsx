9a668c523a69c3e5eeaa726bf1440ddf21c18164
sandeep-bodapudi <sandeepkumarbodapudi07@gmail.com>
2026-05-12 13:18:03 +0530

    fixed build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './sections/Hero';
 import TechStack from './sections/TechStack';
